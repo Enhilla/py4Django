78595f4d12d29213e1e46b965f4d4a5713230711
Enhilla <timopheyenhill@gmail.com>
2026-02-08 18:45:09 +0500

    bd

--- hilla/hilla/settings.py
+++ hilla/hilla/settings.py
@@ -108,12 +108,22 @@ WSGI_APPLICATION = "hilla.wsgi.application"
 # --------------------
 # DATABASE
 # --------------------
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+try:
+    import dj_database_url
+except Exception:
+    dj_database_url = None
+
+if dj_database_url and os.environ.get("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(conn_max_age=600, ssl_require=True),
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 # --------------------
 # PASSWORD VALIDATION
